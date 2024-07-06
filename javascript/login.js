60bf1703095edc3cba2bb2f9baf069ece1f7ad5b
ftinamni <ftinamnii@gmail.com>
2024-07-07 00:37:10 +0800

    Update login.js

---
+++ javascript/login.js
@@ -0,0 +1,10 @@
+function Login() {
+    const usernameInput = ProtectLah.getElementByID("Username");
+    const passwordInput = Protectlah.getElementByID("Password");
+
+    const Username = usernameInput.value;
+    const Password = passwordInput.value;
+}
+console.log(Username);
+console.log(Password);
+result.innerHTML=`Welcome user ${Username}`;
